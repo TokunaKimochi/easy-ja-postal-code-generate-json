7028eae99ff7c4fede8683e9614cc21771d839f2
underground0930 <go.nishiduka.1985@gmail.com>
2023-03-31 18:41:55 +0900

    update

---
+++ package/src/type.d.ts
@@ -0,0 +1,10 @@
+export type ZipIndex = string[];
+export type ZipData = {
+    zip: string;
+    prefectures: string;
+    city: string;
+    other: string;
+};
+export type ZipAllData = {
+    [key: string]: ZipData[];
+};
